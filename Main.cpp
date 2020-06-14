c16b5d252f17742880623370a8a55a2cfcc0eb31
Mashhch <56107606+Mashhch@users.noreply.github.com>
2020-06-14 05:37:10 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,103 +6,7 @@
 #include "BinarTree.h"
 #include <utility>
 
-
-void TestBinarHeapAdd() {
-	{
-		BinaryHeap<int> h(7);
-		int tmp[1] = { 7 };
-		int* temp = new int[h.getSize()];
-		for (int j = 0; j < h.getSize(); j++) {
-			temp[j] = h.searchValue(j);
-		}
-		for (int j = 0; j < h.getSize(); j++)
-			assert(tmp[j] == temp[j]);
-	}
-	{
-		BinaryHeap<int> h((3, 0));
-		for (auto i = 2; i < 5; i++) h.Add((i, 0));
-		std::complex<int> tmp[4] = { (4,0),(3,0),(3,0),(2,0) };
-		std::complex<int>* temp = new std::complex<int>[h.getSize()];
-		for (int j = 0; j < h.getSize(); j++) {
-			temp[j] = h.searchValue(j);
-		}
-		for (int j = 0; j < h.getSize(); j++)
-			assert(tmp[j] == temp[j]);
-	}
-	{
-		double temp[6] = { 2.11, 0.53, 1.52, 2.5, 3.71, 4 };
-		BinaryHeap<double> h(temp, 6);
-		double tmp[6] = { 4, 2.5 ,3.71, 0.53, 2.11, 1.52 };
-		for (int j = 0; j < h.getSize(); j++) {
-			temp[j] = h.searchValue(j);
-		}
-		for (int j = 0; j < h.getSize(); j++)
-			assert(tmp[j] == temp[j]);
-	}
-}
-	void TestBinarHeapDelElem() {
-		{
-			BinaryHeap<int> h(2);
-			for (auto i = 0; i < 5; i++) h.Add(i);
-			h.deleteIn(0);
-			int tmp[5] = { 3,2,2,0,1 };
-			int* temp = new int[5];
-			temp[0] = 0;
-			for (int j = 0; j < h.getSize(); j++) {
-				temp[j] = h.searchValue(j);
-			}
-			for (int j = 0; j < h.getSize(); j++)
-				assert(tmp[j] == temp[j]);
-		}
-		{
-			BinaryHeap<int> h(2);
-			for (auto i = 0; i < 5; i++) h.Add(i);
-			h.deleteIn(3);
-			int tmp[5] = { 4,2,3,1,2 };
-			int* temp = new int[5];
-			temp[0] = 0;
-			for (int j = 0; j < h.getSize(); j++) {
-				temp[j] = h.searchValue(j);
-			}
-			for (int j = 0; j < h.getSize(); j++)
-				assert(tmp[j] == temp[j]);
-		}
-	}
-
-	void TestBinarHeapFindElem(){
-		{
-		BinaryHeap<int> h(2);
-		for (auto i = 0; i < 5; i++) h.Add(i);
-		int temp = h.searchValue(3);
-		int tmp = 0;
-		assert(tmp == temp);
-	}
-	{
-		BinaryHeap<int> h(2);
-		for (auto i = 0; i < 5; i++) h.Add(i);
-		int temp = h.searchIndex(0);
-		int tmp = 3;
-		assert(tmp == temp);
-	}
-	{
-		double temp[6] = { 2.1,0.3,1.22,2.5,3.01,4 };
-		BinaryHeap<double> h(temp, 6);
-		double temp_ = h.getMin();
-		double tmp = 0.3;
-		assert(tmp == temp_);
-	}
-
-	{
-		BinaryHeap<int> h(2);
-		for (auto i = 0; i < 5; i++) h.Add(i);
-		int temp = h.getMax();
-		int tmp = 4;
-		assert(tmp == temp);
-	}
-
-}
-
-	void TestBinarTreeAdd() {
+void TestBinarTreeAdd() {
 		{
 			BinaryTree<int> h(0);
 			bool quest = h.exist(0);
@@ -199,6 +103,103 @@ void TestBinarHeapAdd() {
 
 }
 
+
+void TestBinarHeapAdd() {
+	{
+		BinaryHeap<int> h(7);
+		int tmp[1] = { 7 };
+		int* temp = new int[h.getSize()];
+		for (int j = 0; j < h.getSize(); j++) {
+			temp[j] = h.searchValue(j);
+		}
+		for (int j = 0; j < h.getSize(); j++)
+			assert(tmp[j] == temp[j]);
+	}
+	{
+		BinaryHeap<int> h((3, 0));
+		for (auto i = 2; i < 5; i++) h.Add((i, 0));
+		std::complex<int> tmp[4] = { (4,0),(3,0),(3,0),(2,0) };
+		std::complex<int>* temp = new std::complex<int>[h.getSize()];
+		for (int j = 0; j < h.getSize(); j++) {
+			temp[j] = h.searchValue(j);
+		}
+		for (int j = 0; j < h.getSize(); j++)
+			assert(tmp[j] == temp[j]);
+	}
+	{
+		double temp[6] = { 2.11, 0.53, 1.52, 2.5, 3.71, 4 };
+		BinaryHeap<double> h(temp, 6);
+		double tmp[6] = { 4, 2.5 ,3.71, 0.53, 2.11, 1.52 };
+		for (int j = 0; j < h.getSize(); j++) {
+			temp[j] = h.searchValue(j);
+		}
+		for (int j = 0; j < h.getSize(); j++)
+			assert(tmp[j] == temp[j]);
+	}
+}
+	void TestBinarHeapDelElem() {
+		{
+			BinaryHeap<int> h(2);
+			for (auto i = 0; i < 5; i++) h.Add(i);
+			h.deleteIn(0);
+			int tmp[5] = { 3,2,2,0,1 };
+			int* temp = new int[5];
+			temp[0] = 0;
+			for (int j = 0; j < h.getSize(); j++) {
+				temp[j] = h.searchValue(j);
+			}
+			for (int j = 0; j < h.getSize(); j++)
+				assert(tmp[j] == temp[j]);
+		}
+		{
+			BinaryHeap<int> h(2);
+			for (auto i = 0; i < 5; i++) h.Add(i);
+			h.deleteIn(3);
+			int tmp[5] = { 4,2,3,1,2 };
+			int* temp = new int[5];
+			temp[0] = 0;
+			for (int j = 0; j < h.getSize(); j++) {
+				temp[j] = h.searchValue(j);
+			}
+			for (int j = 0; j < h.getSize(); j++)
+				assert(tmp[j] == temp[j]);
+		}
+	}
+
+	void TestBinarHeapFindElem(){
+		{
+		BinaryHeap<int> h(2);
+		for (auto i = 0; i < 5; i++) h.Add(i);
+		int temp = h.searchValue(3);
+		int tmp = 0;
+		assert(tmp == temp);
+	}
+	{
+		BinaryHeap<int> h(2);
+		for (auto i = 0; i < 5; i++) h.Add(i);
+		int temp = h.searchIndex(0);
+		int tmp = 3;
+		assert(tmp == temp);
+	}
+	{
+		double temp[6] = { 2.1,0.3,1.22,2.5,3.01,4 };
+		BinaryHeap<double> h(temp, 6);
+		double temp_ = h.getMin();
+		double tmp = 0.3;
+		assert(tmp == temp_);
+	}
+
+	{
+		BinaryHeap<int> h(2);
+		for (auto i = 0; i < 5; i++) h.Add(i);
+		int temp = h.getMax();
+		int tmp = 4;
+		assert(tmp == temp);
+	}
+
+}
+
+	
 	void TestQueueAdd() {
 		{
 			Queue<int> h(0, 2);
